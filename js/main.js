89f248803b9b67c39639ef93061d076742130b39
RafaelFatkulin <armistman@vk.com>
2023-05-04 11:23:08 +0500

    fixed menu scroll

--- js/main.js
+++ js/main.js
@@ -4,19 +4,9 @@ const menu = document.querySelector('.menu')
 const body = document.querySelector('body')
 
 let isMenuActive = false
-
-// Disabling Scroll
-const preventScroll = (e) => {
-    e.preventDefault()
-    e.stopPropagation()
-
-    return false
-}
 burger.addEventListener('click', () => {
     isMenuActive = !isMenuActive
     burger.classList.toggle('header__burger-active')
     menu.classList.toggle('menu-active')
-    if(isMenuActive) {
-        body.addEventListener('wheel', preventScroll, {passive: false})
-    } else body.removeEventListener('wheel', preventScroll)
+    body.classList.toggle('menu-is-active')
 })
