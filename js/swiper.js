bde58a022e1e03715f982b4241a5633bf42ca8c9
RafaelFatkulin <armistman@vk.com>
2023-05-07 19:39:27 +0500

    Added error messages

--- js/swiper.js
+++ js/swiper.js
@@ -1,11 +1,11 @@
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    slidesPerView: 'auto',
+const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    slidesPerView: "auto",
     loop: true,
     autoplay: true,
     pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
-    }
-})
+        el: ".swiper-pagination",
+        type: "bullets",
+        clickable: true,
+    },
+});
